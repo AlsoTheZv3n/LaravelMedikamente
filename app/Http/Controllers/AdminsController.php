2b325af74e481587301fd47a274a7699ba961b8b
Sven Weidenmann <sweidenmann@gmail.com>
2022-05-27 21:37:25 +0200

    sith commit

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -18,24 +18,24 @@ class AdminsController extends Controller
     }
 
     public function editadmin($id){
-        $post = User::find($id);
-        return view('posts.edit')->with('post', $post);
+        $user = User::find($id);
+        return view('users.edit')->with('user', $user);
 
     }
 
     public function save(Request $request, $id){
-        $post = User::find($id);
-        $post->username = $request->username;
-        $post->role = $request->role;
-        $post->save();
-        return redirect('/admins')->with('success', 'Post Saved');
+        $user = User::find($id);
+        $user->username = $request->username;
+        $user->role = $request->role;
+        $user->save();
+        return redirect('/admins')->with('success', 'user Saved');
 
     }
 
     public function destroy($id){
-        $post = User::find($id);
-        $post->delete();
-        return redirect('/admins')->with('success', 'Post Delete');
+        $user = User::find($id);
+        $user->delete();
+        return redirect('/admins')->with('success', 'user Delete');
 
     }
 
