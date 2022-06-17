e75ca36187279f921720d4290af5ecbff91c9593
Sven Weidenmann <sweidenmann@gmail.com>
2022-06-17 15:00:17 +0200

    final commit

--- app/Http/Controllers/DrugsCreateController.php
+++ app/Http/Controllers/DrugsCreateController.php
@@ -16,6 +16,8 @@ class DrugsCreateController extends Controller
 
         $drug->name = $name;
         $drug->price = $price;
+        $drug->effect = $name;
+        $drug->effect_type = $name;
 
         $drug->user_id = auth()->user()->id;
         $drug->pharma_id = $pharma->id;
